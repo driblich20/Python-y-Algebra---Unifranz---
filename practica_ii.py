37e4ca8abd039d6b0e41ee7e74ffc6bbcfebcebb
Pablo <Pablo>
2016-05-01 13:54:32 -0400

    Practica II - Bucles

---
+++ practica_ii.py
@@ -0,0 +1,21 @@
+'''
+Practica 2:
+Mediante un bucle while, recorrer los primeros 100 numeros enteros, mediante
+un contador.
+si el contador es PAR:
+    añadir en uno, la variable "pares"
+sino:
+    Seguir con el bucle
+Imprimir la variable "enteros"
+
+@Parametros:
+    contador: Numero que aumentara en uno, luego de cada repeticion 
+    pares: Una variable que se inicializa con el valor 0
+              y aumenta en uno si el contador el par
+'''
+contador = 0
+pares = 0
+while contador < 100:
+    ## Por hacer
+
+print pares
